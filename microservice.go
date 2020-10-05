e942e1e814905cc036d204d3c2cfa5221a526d80
sunimalherath <sunimaldv@gmail.com>
2020-10-05 16:27:01 +1100

    implementing simple server

--- microservice.go
+++ microservice.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/api/echo", echo)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Sample server up and running")
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Query()["message"][0]
+	w.Header().Add("Content-Type", "text/plain")
+	fmt.Fprintf(w, message)
 }
